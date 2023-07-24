89ffeb5b89ddbc8b80c606887f6c67a429066178
txxnrd <nrbsld@korea.ac.kr>
2023-07-24 20:23:14 +0900

    0724 20:23

--- UniMate/UniMate/Main/BoardPage.swift
+++ UniMate/UniMate/Main/BoardPage.swift
@@ -10,11 +10,273 @@ import SwiftUI
 struct BoardView: View {
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
+            ScrollView {
+                VStack (alignment: .leading){
+                    HStack {
+                        Text("BEST 게시판")
+                        Spacer()
+                        Button {
+                            print("Go to BEST board")
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .onTapGesture {
+                        // give on tap gesture to entire hstack?
+                        print("Go to BEST board?")
+                    }
+                    
+                    Divider()
+                        .background(Color.black)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text("익명")
+                                    .bold()
+                            }
+                            Text("Title1")
+                                .bold()
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                                .lineLimit(2)
+                        }
+                        
+                        HStack {
+                            Text("자유게시판")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(UIColor(hexCode: "665E5E")))
+                            Spacer()
+                            Label("3", systemImage: "hand.thumbsup")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                            Label("5", systemImage: "message")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        
+                    }
+                    .frame(alignment: .leading)
+                    .padding(10)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text("익명")
+                                    .bold()
+                            }
+                            Text("Title1")
+                                .bold()
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                                .lineLimit(2)
+                        }
+                        
+                        HStack {
+                            Text("자유게시판")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(UIColor(hexCode: "665E5E")))
+                            Spacer()
+                            Label("3", systemImage: "hand.thumbsup")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                            Label("5", systemImage: "message")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        
+                    }
+                    .frame(alignment: .leading)
+                    .padding(10)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text("익명")
+                                    .bold()
+                            }
+                            Text("Title1")
+                                .bold()
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                                .lineLimit(2)
+                        }
+                        
+                        HStack {
+                            Text("자유게시판")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(UIColor(hexCode: "665E5E")))
+                            Spacer()
+                            Label("3", systemImage: "hand.thumbsup")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                            Label("5", systemImage: "message")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        
+                    }
+                    .frame(alignment: .leading)
+                    .padding(10)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text("익명")
+                                    .bold()
+                            }
+                            Text("Title1")
+                                .bold()
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                                .lineLimit(2)
+                        }
+                        
+                        HStack {
+                            Text("자유게시판")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(UIColor(hexCode: "665E5E")))
+                            Spacer()
+                            Label("3", systemImage: "hand.thumbsup")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                            Label("5", systemImage: "message")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        
+                    }
+                    .frame(alignment: .leading)
+                    .padding(10)
+                    
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Image(systemName: "person")
+                                Text("익명")
+                                    .bold()
+                            }
+                            Text("Title1")
+                                .bold()
+                            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.")
+                                .lineLimit(2)
+                        }
+                        
+                        HStack {
+                            Text("자유게시판")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(UIColor(hexCode: "665E5E")))
+                            Spacer()
+                            Label("3", systemImage: "hand.thumbsup")
+                                .font(.system(size: 12))
+                                .foregroundColor(.red)
+                            Label("5", systemImage: "message")
+                                .font(.system(size: 12))
+                                .foregroundColor(.blue)
+                        }
+                        
+                    }
+                    .frame(alignment: .leading)
+                    .padding(10)
+                    
+                    
+                }
+                .padding(10)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(UIColor(hexCode: "969696")), lineWidth: 1)
+                )
+                .padding(.horizontal)
+                
+                VStack (alignment: .leading){
+                    HStack {
+                        Text("게시판 목록")
+                    }
+                
+                    Divider()
+                        .background(Color.black)
+                    
+                    HStack {
+                        Text("자유 게시판")
+                        Spacer()
+                        Button {
+                            print("Go to community board")
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.vertical)
+                    .onTapGesture {
+                        // give on tap gesture to entire hstack?
+                        print("Go to community board?")
+                    }
+                    
+                    
+                    
+                    HStack {
+                        Text("대학원 게시판")
+                        Spacer()
+                        Button {
+                            print("Go to grad board")
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.vertical)
+                    .onTapGesture {
+                        // give on tap gesture to entire hstack?
+                        print("Go to grad board?")
+                    }
+                    
+                    HStack {
+                        Text("동아리 게시판")
+                        Spacer()
+                        Button {
+                            print("Go to club board")
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.vertical)
+                    .onTapGesture {
+                        // give on tap gesture to entire hstack?
+                        print("Go to club board?")
+                    }
+                    
+                    HStack {
+                        Text("홍보 게시판")
+                        Spacer()
+                        Button {
+                            print("Go to ad board")
+                        } label: {
+                            Image(systemName: "chevron.right")
+                        }
+                    }
+                    .padding(.vertical)
+                    .onTapGesture {
+                        // give on tap gesture to entire hstack?
+                        print("Go to ad board?")
+                    }
+                    
+                    
+                    
+                }
+                .padding(10)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(UIColor(hexCode: "969696")), lineWidth: 1)
+                )
+                .padding(.vertical)
+                .padding(.horizontal)
             }
             .navigationTitle("게시판")
             .navigationBarTitleDisplayMode(.inline)
+            
+            
+            
+            
 
         }
     }
--- UniMate/UniMate/Main/UserPage.swift
+++ UniMate/UniMate/Main/UserPage.swift
@@ -1,21 +1,127 @@
-//
-//  UserPage.swift
-//  UniMate
-//
-//  Created by 유석원 on 2023/07/22.
-//
-
 import SwiftUI
+import Firebase
+import UIKit
 
 struct UserView: View {
+    @State private var userID: String = ""
+    @State private var email: String = ""
+    @State private var nickname: String = ""
+    @State private var studentId: String = ""
+    @State private var university: String = ""
+    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var image: Image?
+
     var body: some View {
         NavigationView {
-            VStack {
-                Spacer()
+            ScrollView {
+                VStack(spacing: 20) {
+                    Image(uiImage: self.inputImage ?? UIImage(systemName: "person.circle.fill")!)
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.blue, lineWidth: 3))
+                        .shadow(radius: 10)
+                    .onTapGesture {
+                        self.showingImagePicker = true
+                    }
+                    .padding()
+
+                    UserDetailView(title: "이메일", detail: $email)
+                    UserDetailView(title: "닉네임", detail: $nickname)
+                    UserDetailView(title: "학번", detail: $studentId)
+                    UserDetailView(title: "대학교", detail: $university)
+                }
+                .padding()
+                .onAppear {
+                    loadUserData()
+                }
+                .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                    ImagePicker(image: self.$inputImage)
+                }
+                .navigationTitle("내 정보")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .navigationTitle("내 정보")
-            .navigationBarTitleDisplayMode(.inline)
         }
+    }
+
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+        // 이 시점에서 inputImage를 Firebase에 업로드하면 됩니다.
+    }
+
+    func loadUserData() {
+        if let user = Auth.auth().currentUser {
+            self.userID = user.uid
+            self.email = user.email ?? ""
+            
+            let db = Database.database(url: "https://unimate-16065-default-rtdb.asia-southeast1.firebasedatabase.app").reference().child("users").child(self.userID)
+
+            db.child("nickname").observeSingleEvent(of: .value) { snapshot in
+                self.nickname = snapshot.value as? String ?? ""
+            }
+
+            db.child("studentId").observeSingleEvent(of: .value) { snapshot in
+                self.studentId = snapshot.value as? String ?? ""
+            }
+
+            db.child("university").observeSingleEvent(of: .value) { snapshot in
+                self.university = snapshot.value as? String ?? ""
+            }
+        }
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        var parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct UserDetailView: View {
+    var title: String
+    @Binding var detail: String
+
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text(title)
+                .font(.headline)
+            Text(detail)
+        }
+        .padding()
+        .frame(width: 300)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(10)
+        .shadow(radius: 5)
     }
 }
 

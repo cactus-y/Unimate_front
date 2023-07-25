a8305d3e5131d6c8ee2d518d6359ddd824a0a9f9
txxnrd <nrbsld@korea.ac.kr>
2023-07-25 13:18:58 +0900

    0725 13:18

--- UniMate/UniMate/Main/LeaderBoardPage.swift
+++ UniMate/UniMate/Main/LeaderBoardPage.swift
@@ -1,77 +1,88 @@
 import SwiftUI
 
+struct University: Identifiable {
+    var id = UUID()
+    var name: String
+    var likesCount: Int
+}
+
 struct LeaderBoardView: View {
-    public let RankUniversity = [
-        "고려대",
-        "KAIST",
-        "POSTECH",
-        "GIST",
-        "한양대",
-        "성균관대",
-        "숙명여대"
+    @State var universities = [
+        University(name: "고려대", likesCount: 0),
+        University(name: "KAIST", likesCount: 0),
+        University(name: "POSTECH", likesCount: 0),
+        University(name: "GIST", likesCount: 0),
+        University(name: "한양대", likesCount: 0),
+        University(name: "성균관대", likesCount: 0),
+        University(name: "숙명여대", likesCount: 0)
     ]
-    
+
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading) {
-                    ForEach(RankUniversity.indices, id: \.self) { index in
-                        UniversityView(rank: index + 1, universityName: RankUniversity[index])
+                    ForEach(universities.sorted(by: { $0.likesCount > $1.likesCount }).indices, id: \.self) { index in
+                        let university = universities.sorted(by: { $0.likesCount > $1.likesCount })[index]
+                        UniversityView(rank: index + 1, university: self.$universities[self.getIndex(of: university)])
                     }
                 }
                 .navigationTitle("학교 순위")
             }
         }
     }
+    
+    private func getIndex(of university: University) -> Int {
+        guard let index = universities.firstIndex(where: { $0.id == university.id }) else {
+            fatalError("Index is out of range.")
+        }
+        return index
+    }
 }
 
 struct UniversityView: View {
     var rank: Int
-    var universityName: String
-
-    // State for likes counter
-    @State private var likesCount = 0
+    @Binding var university: University
 
     var body: some View {
         HStack {
             Text("\(rank)")
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .frame(width: 60, alignment: .center) // Set a fixed frame and center alignment
+                .frame(width: 60, alignment: .center)
 
-            Spacer() // Provide some space
+            Spacer()
 
-            Image(universityName)
+            Image(university.name)
                 .resizable()
                 .scaledToFit()
                 .frame(width: 100, height: 100)
 
-            Spacer() // Provide some space
+            Spacer()
 
             VStack {
                 Button(action: {
-                    likesCount += 1
+                    university.likesCount += 1
                 }) {
-                    Image(systemName: "heart") // Using SF Symbols for a heart icon
+                    Image(systemName: "heart")
                         .resizable()
                         .frame(width: 35, height: 35)
-                        .foregroundColor(.red) // Color the heart icon red
+                        .foregroundColor(.red)
                 }
 
-                Text("\(likesCount)")
-                    .frame(width: 100, alignment: .center) // Set a fixed frame and center alignment
+                Text("\(university.likesCount)")
+                    .frame(width: 100, alignment: .center)
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-               .background(Color(.secondarySystemBackground))
-               .cornerRadius(10)
-               .padding(.horizontal)
-               .frame(width: 360,height: 110)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(10)
+        .padding(.horizontal)
+        .frame(width: 360,height: 110)
     }
 }
 
 struct LeaderBoardView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LeaderBoardView()
     }
 }
